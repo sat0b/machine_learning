ef6e0c92772eb558ef6000bdb5431b95add15711
Junichi Sato <sat0b.3ee@gmail.com>
2021-08-29 23:13:24 +0900

    Add a helper function to connect tpu

--- utils/tpu.py
+++ utils/tpu.py
@@ -3,7 +3,7 @@ import os
 import tensorflow as tf
 
 
-def connect_colab_tpu(tpu):
+def connect_colab_tpu():
     resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
     tf.config.experimental_connect_to_cluster(resolver)
     tf.tpu.experimental.initialize_tpu_system(resolver)
