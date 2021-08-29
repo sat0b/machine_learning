63bfd914499b3a59914678e8566a3ee8adf2ca2a
Junichi Sato <sat0b.3ee@gmail.com>
2021-08-29 23:55:13 +0900

    Remove experimental

--- utils/distribute.py
+++ utils/distribute.py
@@ -7,7 +7,7 @@ def colab_tpu_strategy():
     resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
     tf.config.experimental_connect_to_cluster(resolver)
     tf.tpu.experimental.initialize_tpu_system(resolver)
-    strategy = tf.distribute.experimental.TPUStrategy(resolver)
+    strategy = tf.distribute.TPUStrategy(resolver)
     return strategy
 
 
