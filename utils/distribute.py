66ac69a193f15c3637316387bd4cbfaa937c6d21
Junichi Sato <sat0b.3ee@gmail.com>
2021-08-29 23:46:16 +0900

    Add strategy method to use devices

--- utils/distribute.py
+++ utils/distribute.py
@@ -3,7 +3,14 @@ import os
 import tensorflow as tf
 
 
-def connect_colab_tpu():
+def colab_tpu_strategy():
     resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
     tf.config.experimental_connect_to_cluster(resolver)
     tf.tpu.experimental.initialize_tpu_system(resolver)
+    strategy = tf.distribute.experimental.TPUStrategy(resolver)
+    return strategy
+
+
+def gpu_strategy():
+    strategy = tf.distribute.MirroredStrategy()
+    return strategy
